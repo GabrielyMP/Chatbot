d9123306e94fa59bf1ed0e2e70370923e749c45a
Gabriel San Martin <gssm.snf19@uea.edu.br>
2021-05-31 21:40:58 -0400

    Adicionando pasta web na main

---
+++ web/static/index.js
@@ -0,0 +1,67 @@
+chatElement = document.getElementById('chat');
+messageElement = document.getElementById('message');
+buttonElement = document.getElementById('send');
+
+function createMessageBalloon(content, direction) {
+    const messageBalloon = document.createElement('div');
+    const messageBalloonContent = document.createTextNode(content);
+    
+    for(className of ['message-balloon', direction]) {
+        messageBalloon.classList.add(className);
+    }
+
+    messageBalloon.appendChild(messageBalloonContent);
+
+    return messageBalloon;
+}
+
+function addUserResponse(content) {
+    const messageWrapper = chatElement.children[0];
+    
+    const messageBalloon = createMessageBalloon(content, 'right');
+
+    messageWrapper.appendChild(messageBalloon);
+}
+
+function addBotResponse(content) {
+    const messageWrapper = chatElement.children[0];
+    
+    const messageBalloon = createMessageBalloon(content, 'left');
+
+    messageWrapper.appendChild(messageBalloon);
+}
+
+function scrollDownToLast() {
+    const messages = document.querySelectorAll('.message-balloon');
+
+    const lastMessage = messages[messages.length - 1];
+
+    chatElement.scrollTop = lastMessage.offsetTop;
+}
+
+buttonElement.addEventListener('click', function(e) {
+    const content = messageElement.value;
+
+    if(content == '') {
+        console.log('messageElement is empty');
+        return;
+    }
+
+    addUserResponse(content);
+
+    fetch('http://localhost:5000/', { 
+        method: 'POST', 
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ content })
+    })
+    .then(res => res.json())
+    .then(data => {
+        addBotResponse(data.content);
+        scrollDownToLast();
+    });
+
+    messageElement.value = '';
+});
